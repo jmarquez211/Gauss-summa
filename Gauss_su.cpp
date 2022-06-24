442a04af929d738c64b00ed10466bf7d8f2cdd24
jmarquez211 <marquezjq211@gmail.com>
2022-06-24 09:05:04 +0200

    First exercise

---
+++ Gauss_su.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+
+
+int sum = 0; //global integer
+
+/* We're expectating a int number
+and if the number = 0, we print "sum",
+otherwise we introduce a number and the
+programm will sum it and it will decrease
+until we got zero
+*/
+
+
+/* there's no point to return anything since
+we have a global integer, we make the return
+parameter a void
+*/
+
+void fun(int num) {
+    if (num == 0) {
+        cout << sum << endl; // if num = 0, it will print the sum, that is zero because of aritmetic progression
+    }
+
+    else {
+        sum += num; //this will sum a number and the next one
+        num--; // that'll substract one from num every single time we execute if statement
+        fun(num);
+    }
+
+}
+
+int main(){
+    int num ;
+    cout<<"Select the number you want to sum:\n"<<endl;
+    cin >> num;
+    cout<<"\n";
+    cout<<"The result, using Gauss' Formula is:\n"<<endl;
+
+    fun(num);
+    return 0;
+}
